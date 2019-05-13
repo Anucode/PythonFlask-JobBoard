7b25c886f8aae3216966df2ca3430f688bf8562e
Anuradha Kumari <anuradhakumari@live.com>
2019-05-13 16:15:13 +0530

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -73,6 +73,6 @@ def review(employer_id):
         status = request.form['status']
 
         date = datetime.datetime.now().strftime("%m/%d/%Y")
-        a=execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)'(review, rating, title, date, status, employer_id), commit=True)
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',(review, rating, title, date, status, employer_id), commit=True)
         return redirect(url_for('employer',employer_id=employer_id))
         return  render_template('review.html',employer_id=employer_id)
